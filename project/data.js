d66449fbc4fa3356b6db6b1a21160cea652b674f
Mar, Mike <31870347+Mikegmar@users.noreply.github.com>
2019-07-17 23:27:01 -0700

    Update data.js

--- project/data.js
+++ project/data.js
@@ -1,3 +1,31 @@
+document.addEventListener('mousemove', e => {
+	
+	const x = e.pageX;
+	const y = e.pageY;
+	
+	const elm = document.querySelector('.card');
+	const coords = elm.getBoundingClientRect();
+	
+	const elmX = coords.left + (elm.offsetWidth / 2);
+	const elmY = coords.top + (elm.offsetHeight / 2);
+	
+	const angleX = (elmY - y) / 25;
+	const angleY = (elmX - x) / -25;
+	
+	
+	elm.style.transform = `rotateX(${angleX}deg)
+												 rotateY(${angleY}deg)`;
+	
+})
+
+
+
+
+
+
+
+-----------------------------
+
 function setup() {
         if (localStorage.getItem('cryptoSearch') !== null) {
             var cryptoSearch = JSON.parse(localStorage.getItem('cryptoSearch'));
